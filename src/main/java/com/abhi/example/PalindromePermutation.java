bab84e4801af5b3a97be33a0072fd7ddbdac20a8
ajain <Abhishek_Jain7@dell.com>
2017-06-11 15:58:57 -0700

    Changes to be committed: 	new file:   src/main/java/com/abhi/example/PalindromePermutation.java

---
+++ src/main/java/com/abhi/example/PalindromePermutation.java
@@ -0,0 +1,50 @@
+package com.abhi.example;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * You can assume the input string only contains lowercase letters.
+ * 
+ * Examples:
+ * 
+ * "civic" should return true "ivicc" should return true "civil" should return
+ * false "livci" should return false
+ * 
+ * 
+ * noun: palindrome; plural noun: palindromes a word, phrase, or sequence that
+ * reads the same backward as forward, e.g., madam or nurses run.
+ * 
+ * @author abhishek.ajain
+ *
+ */
+public class PalindromePermutation {
+
+	public static void main(String[] args) {
+		String[] strArr = { "civic", "ivicc", "civil", "livci", "a", "aa", "aabb", "zszsxdxdcf" };
+		for(String str:strArr){
+			System.out.println(str+":"+findPalindrome(str));
+		}
+	}
+	
+	public static boolean findPalindrome(String string){
+		int length = string.length();
+		if(length == 0 || length == 1){
+			return true;
+		}
+		char[] chars = string.toCharArray();
+		int remainder  = length%2;
+		
+		Set<Character> set = new HashSet<Character>();
+		for(char singChar: chars){
+			set.add(singChar);
+		}
+		int size = set.size();
+		if(size <= (Math.abs(length/2)+remainder)){
+			return true;
+		}
+		return false;
+	}
+
+}
