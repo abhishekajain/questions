b9827be154d46d086fa3e3e575b011e28cc3345b
ajain <Abhishek_Jain7@dell.com>
2017-08-19 20:36:14 -0700

    Changes to be committed: 	modified:   src/main/java/com/abhi/datastructure/arrary/ArrayUtil.java

--- src/main/java/com/abhi/datastructure/arrary/ArrayUtil.java
+++ src/main/java/com/abhi/datastructure/arrary/ArrayUtil.java
@@ -1,5 +1,13 @@
 package com.abhi.datastructure.arrary;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder.In;
+
+import org.junit.Test;
+
 public class ArrayUtil {
 	
 	static int[] mergeSortedAaary(int[] a, int[] b){
@@ -40,11 +48,63 @@ public class ArrayUtil {
 		return c;		
 	}
 	
-	public static void main(String[] args) {
+	@Test
+	public void testMergeSortedArray() {
 		int[] a = {1,2,3,4,5};
 		int[] b = {3,5,8};
 
-		int[] c = ArrayUtil.mergeSortedAaary(a, b);
+		int[] c = mergeSortedAaary(a, b);
 		System.out.println(c);
+	}
+	
+	static List<int[]> combinations(int amount, List<Integer> denominations){
+		List<int[]> results = new ArrayList<int[]>();
+		Collections.sort(denominations);
+		for(Integer denomination:denominations){
+			if(amount<denomination){
+				continue;
+			}
+			int rem = amount%denomination;
+			int howMany = amount/denomination;
+			int[] resultMany = new int[howMany];
+			for(int i=0; i<resultMany.length; i++){
+				resultMany[i] = denomination;
+			}
+
+			if(rem == 0){
+				results.add(resultMany);
+			}else{
+				//Recursion for remaining money
+				List<Integer> newList = new ArrayList<Integer>(denominations);
+				newList.remove(denomination);
+				List<int[]> newResults = combinations(rem, newList);
+				
+				//merge results
+				for(int[] result:newResults){
+					int[] newResultsArr = new int[howMany+result.length];
+					for(int i=0; i<resultMany.length;i++){
+						newResultsArr[i] = resultMany[i];
+					}
+					int j = 0;
+					for(int i=resultMany.length; i<newResultsArr.length; i++){
+						newResultsArr[i] = result[j];
+						j++;
+					}
+					results.add(newResultsArr);
+				}
+			}
+		}
+		return results;
+		
+	}
+	
+	@Test
+	public void testCombinations(){
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		List<int[]> results = combinations(32, list);
+		System.out.println(results);
 	}
 }
