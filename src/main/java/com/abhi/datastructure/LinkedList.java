244719e07ceb3dd74fd05b2b86006846f73e629b
ajain <Abhishek_Jain7@dell.com>
2017-08-25 05:55:41 -0700

    Changes to be committed: 	modified:   src/main/java/com/abhi/datastructure/LinkedList.java 	modified:   src/main/java/com/abhi/datastructure/arrary/ArrayUtil.java

--- src/main/java/com/abhi/datastructure/LinkedList.java
+++ src/main/java/com/abhi/datastructure/LinkedList.java
@@ -77,8 +77,8 @@ public class LinkedList {
 			return;
 		}
 		if(head != null){
-			System.out.println(head.value);
-			System.out.println(head.next);
+			System.out.print(head.value);
+			System.out.print("-->");
 			print(head.next);
 		}
 	}
@@ -96,6 +96,136 @@ public class LinkedList {
 
 		Assert.assertTrue(containsCycle(head));
 		Assert.assertEquals(Integer.MAX_VALUE, size(head));
+	}
+	
+//	Reverse a linked list from position m to n. Do it in-place and in one-pass.
+//	For example:
+//	Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+//	return 1->4->3->2->5->NULL.
+	public static LinkedListNode reverseLinkList(LinkedListNode head){
+		if(head == null){
+			return head;
+		}
+		LinkedListNode previous = null;
+		LinkedListNode current = head;
+		LinkedListNode next = null;
+		while(current != null){
+			next = current.next;
+			current.next = previous;
+			previous = current;
+			current = next;
+		}
+		head = previous;
+		return head;
+	}
+	
+	// A simple and tail recursive function to reverse
+    // a linked list.  prev is passed as NULL initially.
+    static void reverseUtil(LinkedListNode curr, LinkedListNode prev) {		
+    	if(curr.next == null){
+        	curr.next = prev;
+        	return;
+    	}
+    	
+    	LinkedListNode next = curr.next;
+    	curr.next = prev;
+    	reverseUtil(next, curr);
+		
+    }
+    
+//    you are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+//    		Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+//    		Output: 7 -> 0 -> 8
+
+//    		    342 + 465 = 807
+//    		Make sure there are no trailing zeros in the output list
+//    		So, 7 -> 0 -> 8 -> 0 is not a valid response even though the value is still 807.
+    
+	public LinkedListNode addTwoNumbers(LinkedListNode a, LinkedListNode b) {
+		LinkedListNode current1 = a; 
+		LinkedListNode current2 = b; 
+		LinkedListNode next1 = null;
+		LinkedListNode next2 = null;
+
+    	int headVal = current1.value+current2.value;
+    	LinkedListNode current = new LinkedListNode(headVal%10);
+    	LinkedListNode next = null;
+    	LinkedListNode head = current;
+    	int nextSum = headVal/10;
+    	while(current1.next != null || current2.next != null){
+    		next1 = current1.next;
+    		int value1 = 0;
+    		if(next1 != null){
+        		current1 = next1;
+        		value1 = next1.value;
+    		}
+    		next2 = current2.next;
+    		int value2 = 0;
+
+    		if(next2 != null){
+        		current2 = next2;
+        		value2 = next2.value;
+
+    		}
+    		
+    		int value = value1+value2+nextSum;
+    		next = new LinkedListNode(value%10);
+    		nextSum = value/10;
+    		current.next = next;
+    		current = next;
+    	}
+        
+        if(nextSum != 0){
+            next = new LinkedListNode(nextSum);
+    		current.next = next;
+        }
+    	return head;		
+	    
+	}
+    
+    @Test
+    public void testAdd(){
+    	LinkedList list1 = new LinkedList();
+    	LinkedList list2 = new LinkedList();
+    	
+		LinkedListNode head = new LinkedListNode(2);
+		head.next = new LinkedListNode(4);
+		head.next.next = new LinkedListNode(3);
+		list1.head = head;
+
+		LinkedListNode head2 = new LinkedListNode(5);
+		head2.next = new LinkedListNode(6);
+//		head2.next.next = new LinkedListNode(4);
+		list2.head = head2;
+		print(head);
+		print(head2);
+		print(addTwoNumbers(head, head2));
+    }
+ 
+    // prints content of double linked list
+    static void printList(LinkedListNode node) {
+        while (node != null) {
+            System.out.print(node.value + "->>");
+            node = node.next;
+        }
+    }
+    
+	public static LinkedListNode reverseLinkList(LinkedListNode head, int m){
+
+		return head;
+	}
+	
+	@Test
+	public void testReverseLinkList(){
+		LinkedListNode head = new LinkedListNode(1);
+		head.next = new LinkedListNode(2);
+		head.next.next = new LinkedListNode(3);
+		head.next.next.next = new LinkedListNode(4);
+		head.next.next.next.next = new LinkedListNode(5);
+		print(head);
+		head = reverseLinkList(head);
+		print(head);
 	}
 
 }
--- src/main/java/com/abhi/datastructure/arrary/ArrayUtil.java
+++ src/main/java/com/abhi/datastructure/arrary/ArrayUtil.java
@@ -7,6 +7,8 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.sun.org.apache.xalan.internal.xsltc.util.IntegerArray;
+
 public class ArrayUtil {
 
 	static Integer[] mergeSortedAaary(int[] a, int[] b) {
@@ -325,10 +327,25 @@ public class ArrayUtil {
 		return maxLength;
 	}
 	
+	public static int longestIncreasingSequence(final List<Integer> a){
+		int[] input = new int[a.size()];
+		for(int i = 0 ; i<a.size(); i++){
+			input[i] = a.get(i);
+		}
+		return longestIncreasingSequence(input);
+	}
+	
 	@Test
 	public void testLongestIncreasingSequence(){
 		int[] input = {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
 		Assert.assertEquals(6, longestIncreasingSequence(input));
+		
+		int[] input2 = { 69, 54, 19, 51, 16, 54, 64, 89, 72, 40, 31, 43, 1, 11, 82, 65, 75, 67, 25, 98, 31, 77, 55, 88, 85, 76, 35, 101, 44, 74, 29, 
+				94, 72, 39, 20, 24, 23, 66, 16, 95, 5, 17, 54, 89, 93, 10, 7, 88, 68, 10, 11, 22, 25, 50, 18, 59, 79, 87, 7, 49, 26, 96, 27, 19, 67, 35, 
+				50, 10, 6, 48, 38, 28, 66, 94, 60, 27, 76, 4, 43, 66, 14, 8, 78, 72, 21, 56, 34, 90, 89 };
+	
+		Assert.assertEquals(15, longestIncreasingSequence(input2));
+
 	}
 	private static int[][] createMaxSeq(int[][] input, int element){
 		if(input == null){
