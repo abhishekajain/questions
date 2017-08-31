72b80a12aa6fe206e2dbb5f17ce9c4e8aa3eeab4
ajain <Abhishek_Jain7@dell.com>
2017-08-31 11:09:54 -0700

    Changes to be committed: 	new file:   src/main/java/com/abhi/datastructure/MaxStack.java 	new file:   src/main/java/com/abhi/datastructure/QueueTwoStacks.java

---
+++ src/main/java/com/abhi/datastructure/QueueTwoStacks.java
@@ -0,0 +1,29 @@
+package com.abhi.datastructure;
+
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+public class QueueTwoStacks<T> {
+
+	Stack<T> stackPush = new Stack<T>();
+	Stack<T> stackPop = new Stack<T>();
+
+	T enqueue(T element) {
+		return stackPush.push(element);
+	}
+
+	T dequeue() {
+		if (stackPop.isEmpty()) {
+			while (stackPush.size() > 1) {
+				stackPop.push(stackPush.pop());
+			}
+			if (stackPush.size() == 1) {
+				return stackPush.pop();
+			} else {
+				throw new NoSuchElementException("empty queue!");
+			}
+		} else {
+			return stackPop.pop();
+		}
+	}
+}
