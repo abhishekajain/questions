a352397742c9904494d215f09ffdfaaae150a36a
ajain <Abhishek_Jain7@dell.com>
2017-08-31 11:08:25 -0700

    Changes to be committed: 	new file:   src/main/java/com/abhi/algo/ReverseString.java 	modified:   src/main/java/com/abhi/datastructure/Boggle.java 	modified:   src/main/java/com/abhi/datastructure/LinkedList.java 	deleted:    src/main/java/com/abhi/datastructure/ReverseString.java 	modified:   src/main/java/com/abhi/datastructure/TwoEgg.java

--- src/main/java/com/abhi/datastructure/Boggle.java
+++ src/main/java/com/abhi/datastructure/Boggle.java
@@ -5,49 +5,47 @@ import java.util.List;
 
 public class Boggle {
 
-	static String[] dictionary = {"GEEKS", "FOR", "QUIZ", "GO"};
+	static String[] dictionary = { "GEEKS", "FOR", "QUIZ", "GO" };
 	static List<String> dictionaryList = Arrays.asList(dictionary);
 
-	
-	static char[][] boggle   = {{'G','I','Z'},
-            					{'U','E','K'},
-            					{'Q','S','E'}};
-	
-	static boolean isWord(String str){
+	static char[][] boggle = { { 'G', 'I', 'Z' }, { 'U', 'E', 'K' }, { 'Q', 'S', 'E' } };
+
+	static boolean isWord(String str) {
 		return dictionaryList.contains(str);
 	}
-	
-	static void createWords(){
+
+	static void createWords() {
 		int m = boggle.length;
 		int n = boggle[0].length;
-//		boolean[][] visted = new boolean[m][n];
-		for(int i=0; i<boggle.length; i++){
+		// boolean[][] visted = new boolean[m][n];
+		for (int i = 0; i < boggle.length; i++) {
 			char[] newChars = boggle[i];
-			for(int j=0; j<newChars.length; j++){
+			for (int j = 0; j < newChars.length; j++) {
 				createWordsUtil(new boolean[m][n], i, j, "");
 			}
 		}
 	}
-	
+
 	static void createWordsUtil(boolean[][] visited, int i, int j, String str) {
 		visited[i][j] = true;
-		str = str+boggle[i][j];
-		if(isWord(str)){
+		str = str + boggle[i][j];
+		if (isWord(str)) {
 			System.out.println(str);
 		}
 		// Traverse 8 adjacent cells of boggle[i][j]
-	    for (int row=i-1; row<=i+1 && row<boggle.length; row++){
-	      for (int col=j-1; col<=j+1 && col<boggle[0].length; col++){
-	        if (row>=0 && col>=0 && !visited[row][col]){
-	        	createWordsUtil(visited, row, col, str);
-	        }
-	      }
-	    }
-	    // remove last char from string and mark visited of current cell as false so that loop can start from next direction
-	    str = str.substring(0, str.length()-1);
-	    visited[i][j] = false;
+		for (int row = i - 1; row <= i + 1 && row < boggle.length; row++) {
+			for (int col = j - 1; col <= j + 1 && col < boggle[0].length; col++) {
+				if (row >= 0 && col >= 0 && !visited[row][col]) {
+					createWordsUtil(visited, row, col, str);
+				}
+			}
+		}
+		// remove last char from string and mark visited of current cell as
+		// false so that loop can start from next direction
+		str = str.substring(0, str.length() - 1);
+		visited[i][j] = false;
 	}
-	
+
 	public static void main(String[] args) {
 		createWords();
 	}
--- src/main/java/com/abhi/datastructure/LinkedList.java
+++ src/main/java/com/abhi/datastructure/LinkedList.java
@@ -7,12 +7,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class LinkedList {
-	
-	static class LinkedListNode{
+
+	static class LinkedListNode {
 		public int value;
 		public LinkedListNode next;
-		
-		public LinkedListNode(int value){
+
+		public LinkedListNode(int value) {
 			this.value = value;
 			this.next = null;
 		}
@@ -43,48 +43,48 @@ public class LinkedList {
 			if (value != other.value)
 				return false;
 			return true;
-		}		
-		
+		}
+
 	}
-	
-	public LinkedListNode head;//firstNode
-	
-	public static boolean containsCycle(LinkedListNode head){
+
+	public LinkedListNode head;// firstNode
+
+	public static boolean containsCycle(LinkedListNode head) {
 		Set<LinkedListNode> set = new HashSet<LinkedListNode>();
-		
-		while(head != null){
+
+		while (head != null) {
 			set.add(head);
 			head = head.next;
-			if(set.contains(head)){
+			if (set.contains(head)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	public static int size(LinkedListNode head){
+
+	public static int size(LinkedListNode head) {
 		int size = 0;
-		while(head != null && size != Integer.MAX_VALUE){
+		while (head != null && size != Integer.MAX_VALUE) {
 			size++;
 			head = head.next;
 		}
 		return size;
 	}
-	
-	public static void print(LinkedListNode head){
-		if(head == null){
+
+	public static void print(LinkedListNode head) {
+		if (head == null) {
 			System.out.println("end");
 			return;
 		}
-		if(head != null){
+		if (head != null) {
 			System.out.print(head.value);
 			System.out.print("-->");
 			print(head.next);
 		}
 	}
-	
+
 	@Test
-	public void testContainsCycle(){
+	public void testContainsCycle() {
 		LinkedListNode head = new LinkedListNode(0);
 		head.next = new LinkedListNode(1);
 		head.next.next = new LinkedListNode(2);
@@ -92,24 +92,25 @@ public class LinkedList {
 		Assert.assertEquals(3, size(head));
 		Assert.assertFalse(containsCycle(head));
 		head.next.next.next = head.next;
-//		print(head);
+		// print(head);
 
 		Assert.assertTrue(containsCycle(head));
 		Assert.assertEquals(Integer.MAX_VALUE, size(head));
 	}
-	
-//	Reverse a linked list from position m to n. Do it in-place and in one-pass.
-//	For example:
-//	Given 1->2->3->4->5->NULL, m = 2 and n = 4,
-//	return 1->4->3->2->5->NULL.
-	public static LinkedListNode reverseLinkList(LinkedListNode head){
-		if(head == null){
+
+	// Reverse a linked list from position m to n. Do it in-place and in
+	// one-pass.
+	// For example:
+	// Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+	// return 1->4->3->2->5->NULL.
+	public static LinkedListNode reverseLinkList(LinkedListNode head) {
+		if (head == null) {
 			return head;
 		}
 		LinkedListNode previous = null;
 		LinkedListNode current = head;
 		LinkedListNode next = null;
-		while(current != null){
+		while (current != null) {
 			next = current.next;
 			current.next = previous;
 			previous = current;
@@ -118,39 +119,40 @@ public class LinkedList {
 		head = previous;
 		return head;
 	}
-//	Reverse a linked list from position m to n. Do it in-place and in one-pass.
-//	For example:
-//	Given 1->2->3->4->5->NULL, m = 2 and n = 4,
-//	return 1->4->3->2->5->NULL.	
-	public static LinkedListNode reverseLinkList(LinkedListNode head, int m, int n){
-		if(head == null){
+
+	// Reverse a linked list from position m to n. Do it in-place and in
+	// one-pass.
+	// For example:
+	// Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+	// return 1->4->3->2->5->NULL.
+	public static LinkedListNode reverseLinkList(LinkedListNode head, int m, int n) {
+		if (head == null) {
 			return head;
 		}
-		if(n<=m){
-			return head;//nothing to do//what will happen when m =1
+		if (n <= m) {
+			return head;// nothing to do//what will happen when m =1
 		}
 		LinkedListNode previous = null;
 		LinkedListNode current = head;
 		LinkedListNode next = null;
 		LinkedListNode headToTrack = null;
 		int index = 1;
-		while(current != null && index<m){
+		while (current != null && index < m) {
 			index++;
 			previous = current;
 			current = current.next;
 		}
-		if(m==1){
-		    head = reverseLinkList(current, n);    
+		if (m == 1) {
+			head = reverseLinkList(current, n);
+		} else {
+			previous.next = reverseLinkList(current, n);
 		}
-		else{
-		    previous.next = reverseLinkList(current, n);
-		}
-//		head = previous;
+		// head = previous;
 		return head;
 	}
-	
-	public static LinkedListNode reverseLinkList(LinkedListNode head, int n){
-		if(head == null){
+
+	public static LinkedListNode reverseLinkList(LinkedListNode head, int n) {
+		if (head == null) {
 			return head;
 		}
 		LinkedListNode previous = null;
@@ -158,7 +160,7 @@ public class LinkedList {
 		LinkedListNode next = null;
 		int index = 1;
 
-		while(current != null && index<n){
+		while (current != null && index < n) {
 			index++;
 			next = current.next;
 			current.next = previous;
@@ -169,77 +171,80 @@ public class LinkedList {
 		head = previous;
 		return head;
 	}
-	
+
 	// A simple and tail recursive function to reverse
-    // a linked list.  prev is passed as NULL initially.
-    static void reverseUtil(LinkedListNode curr, LinkedListNode prev) {		
-    	if(curr.next == null){
-        	curr.next = prev;
-        	return;
-    	}
-    	
-    	LinkedListNode next = curr.next;
-    	curr.next = prev;
-    	reverseUtil(next, curr);
-		
-    }
-    
-//    you are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+	// a linked list. prev is passed as NULL initially.
+	static void reverseUtil(LinkedListNode curr, LinkedListNode prev) {
+		if (curr.next == null) {
+			curr.next = prev;
+			return;
+		}
 
-//    		Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-//    		Output: 7 -> 0 -> 8
+		LinkedListNode next = curr.next;
+		curr.next = prev;
+		reverseUtil(next, curr);
 
-//    		    342 + 465 = 807
-//    		Make sure there are no trailing zeros in the output list
-//    		So, 7 -> 0 -> 8 -> 0 is not a valid response even though the value is still 807.
-    
+	}
+
+	// you are given two linked lists representing two non-negative numbers. The
+	// digits are stored in reverse order and each of their nodes contain a
+	// single digit. Add the two numbers and return it as a linked list.
+
+	// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+	// Output: 7 -> 0 -> 8
+
+	// 342 + 465 = 807
+	// Make sure there are no trailing zeros in the output list
+	// So, 7 -> 0 -> 8 -> 0 is not a valid response even though the value is
+	// still 807.
+
 	public LinkedListNode addTwoNumbers(LinkedListNode a, LinkedListNode b) {
-		LinkedListNode current1 = a; 
-		LinkedListNode current2 = b; 
+		LinkedListNode current1 = a;
+		LinkedListNode current2 = b;
 		LinkedListNode next1 = null;
 		LinkedListNode next2 = null;
 
-    	int headVal = current1.value+current2.value;
-    	LinkedListNode current = new LinkedListNode(headVal%10);
-    	LinkedListNode next = null;
-    	LinkedListNode head = current;
-    	int nextSum = headVal/10;
-    	while(current1.next != null || current2.next != null){
-    		next1 = current1.next;
-    		int value1 = 0;
-    		if(next1 != null){
-        		current1 = next1;
-        		value1 = next1.value;
-    		}
-    		next2 = current2.next;
-    		int value2 = 0;
+		int headVal = current1.value + current2.value;
+		LinkedListNode current = new LinkedListNode(headVal % 10);
+		LinkedListNode next = null;
+		LinkedListNode head = current;
+		int nextSum = headVal / 10;
+		while (current1.next != null || current2.next != null) {
+			next1 = current1.next;
+			int value1 = 0;
+			if (next1 != null) {
+				current1 = next1;
+				value1 = next1.value;
+			}
+			next2 = current2.next;
+			int value2 = 0;
 
-    		if(next2 != null){
-        		current2 = next2;
-        		value2 = next2.value;
+			if (next2 != null) {
+				current2 = next2;
+				value2 = next2.value;
 
-    		}
-    		
-    		int value = value1+value2+nextSum;
-    		next = new LinkedListNode(value%10);
-    		nextSum = value/10;
-    		current.next = next;
-    		current = next;
-    	}
-        
-        if(nextSum != 0){
-            next = new LinkedListNode(nextSum);
-    		current.next = next;
-        }
-    	return head;		
-	    
+			}
+
+			int value = value1 + value2 + nextSum;
+			next = new LinkedListNode(value % 10);
+			nextSum = value / 10;
+			current.next = next;
+			current = next;
+		}
+
+		if (nextSum != 0) {
+			next = new LinkedListNode(nextSum);
+			current.next = next;
+		}
+		return head;
+
 	}
-    
-    @Test
-    public void testAdd(){
-    	LinkedList list1 = new LinkedList();
-    	LinkedList list2 = new LinkedList();
-    	
+
+	@Test
+	public void testAdd() {
+		LinkedList list1 = new LinkedList();
+		LinkedList list2 = new LinkedList();
+
 		LinkedListNode head = new LinkedListNode(2);
 		head.next = new LinkedListNode(4);
 		head.next.next = new LinkedListNode(3);
@@ -247,36 +252,37 @@ public class LinkedList {
 
 		LinkedListNode head2 = new LinkedListNode(5);
 		head2.next = new LinkedListNode(6);
-//		head2.next.next = new LinkedListNode(4);
+		// head2.next.next = new LinkedListNode(4);
 		list2.head = head2;
 		print(head);
 		print(head2);
 		print(addTwoNumbers(head, head2));
-    }
- 
-    // prints content of double linked list
-    static void printList(LinkedListNode node) {
-        while (node != null) {
-            System.out.print(node.value + "->>");
-            node = node.next;
-        }
-    }
-    
+	}
+
+	// prints content of double linked list
+	static void printList(LinkedListNode node) {
+		while (node != null) {
+			System.out.print(node.value + "->>");
+			node = node.next;
+		}
+	}
+
 	@Test
-	public void testReverseLinkListWithBound(){
+	public void testReverseLinkListWithBound() {
 		LinkedListNode head = new LinkedListNode(1);
 		head.next = new LinkedListNode(2);
 		head.next.next = new LinkedListNode(3);
-//		head.next.next.next = new LinkedListNode(4);
-//		head.next.next.next.next = new LinkedListNode(5);
-		System.out.println("testReverseLinkListWithBound-->");print(head);
+		// head.next.next.next = new LinkedListNode(4);
+		// head.next.next.next.next = new LinkedListNode(5);
+		System.out.println("testReverseLinkListWithBound-->");
+		print(head);
 		head = reverseLinkList(head, 1, 2);
 		print(head);
 		System.out.println("testReverseLinkListWithBound-->");
 	}
-	
+
 	@Test
-	public void testReverseLinkList(){
+	public void testReverseLinkList() {
 		LinkedListNode head = new LinkedListNode(1);
 		head.next = new LinkedListNode(2);
 		head.next.next = new LinkedListNode(3);
