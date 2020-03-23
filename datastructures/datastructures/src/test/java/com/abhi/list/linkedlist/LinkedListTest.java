fe1533f1dc723cbed8b1f3bc35d7e575b78e6999
Abhishek Jain <Abhishek_Jain7@DELL.COM>
2020-03-23 13:44:22 -0700

    LinkedList

---
+++ datastructures/datastructures/src/test/java/com/abhi/list/linkedlist/LinkedListTest.java
@@ -0,0 +1,26 @@
+package com.abhi.list.linkedlist;
+
+import org.junit.Test;
+
+public class LinkedListTest {
+	
+    @Test
+    public void testAddToFront() {
+        LinkedList list = new LinkedList();
+        list.addToFront(5);
+        list.addToFront(15);
+        list.addToFront(8);
+        list.addToFront(10);
+        list.addToFront(11);
+
+        list.print();
+        System.out.println();
+        System.out.println(list.removeFromFront().getValue());
+        list.print();
+        System.out.println();
+        System.out.println(list.removeFromEnd().getValue());
+        list.print();
+        assert true;
+    }
+
+}
